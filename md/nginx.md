f871189ef2d90e05ced31dba2cf644a080c5ccaa
朱绍能(10030357) <zhusn-b@glodon.com>
2019-12-16 18:56:24 +0800

    addcodes

---
+++ md/nginx.md
@@ -0,0 +1,6 @@
+/usr/local/nginx/sbin/nginx -s reload 重启
+
+netstat -anp | grep :80 //Nginx监听的是80端口，那么就可以查看80端口运行的程序来判断Nginx是否运行
+/usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf
+/usr/local/nginx/sbin/nginx -s reload
+./nginx -s reload // nginx 重启
--- project/a/gulpfile.js
+++ project/a/gulpfile.js
@@ -3,9 +3,9 @@ var sftp = require('gulp-sftp')
 gulp.task('default', function () {
   return gulp.src('web/**/*')
     .pipe(sftp({
-      host: '47.101.198.211',
-      user: 'root',
-      pass: 'Choulun@354266',
-      remotePath: '/opt/webapp/pages/web/'
+      host: '', // 服务器IP例如 47.245.18.226
+      user: 'root', // 服务器用户名
+      pass: '', // 密码
+      remotePath: '/opt/webapp/pages/web/' // 打包服务器路径
     }))
 })
